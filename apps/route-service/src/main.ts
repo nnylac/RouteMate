1fd502d8046abd46c6572561c46e2a53703d8df4
nnylac <calynnongyx@gmail.com>
2026-03-15 14:30:31 +0800

    removed route-service

--- apps/route-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RouteServiceModule } from './route-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RouteServiceModule);
-  await app.listen(process.env.port ?? 3008);
-}
-bootstrap();
--- apps/route-service/src/route-service.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class RouteServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
