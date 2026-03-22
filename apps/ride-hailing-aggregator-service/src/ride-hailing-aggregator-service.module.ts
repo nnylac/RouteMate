7ba315d3c7c2d67f2f7ca83cbf786104c53f8948
jieyinglim2024-art <jieying.lim.2024@smu.edu.sg>
2026-03-22 17:11:22 +0800

    feat: complete wrapper and aggregator sorting logic

--- apps/ride-hailing-aggregator-service/src/ride-hailing-aggregator-service.module.ts
+++ apps/ride-hailing-aggregator-service/src/ride-hailing-aggregator-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RideHailingAggregatorServiceController } from './ride-hailing-aggregator-service.controller';
 import { RideHailingAggregatorServiceService } from './ride-hailing-aggregator-service.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [RideHailingAggregatorServiceController],
   providers: [RideHailingAggregatorServiceService],
 })
