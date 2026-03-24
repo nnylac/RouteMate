fd9a17e5066e6f9e136f60cbdf762cf3290a2d62
lesleytan2024-sudo <lesley.tan.2024@computing.smu.edu.sg>
2026-03-24 12:07:28 +0800

    done

--- apps/maps-wrapper-service/src/maps-wrapper-service.module.ts
+++ apps/maps-wrapper-service/src/maps-wrapper-service.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MapsWrapperServiceController } from './maps-wrapper-service.controller';
 import { MapsWrapperServiceService } from './maps-wrapper-service.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [MapsWrapperServiceController],
   providers: [MapsWrapperServiceService],
 })
