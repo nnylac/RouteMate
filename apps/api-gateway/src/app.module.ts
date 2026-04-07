82c6ecdf89aa79f79e213207b624d4914289322d
jieyinglim2024-art <jieying.lim.2024@smu.edu.sg>
2026-04-07 15:51:41 +0800

    fix: api-gateway port conflict, kong routes, mongo config

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { FareServiceModule } from 'apps/fare-service/src/fare-service.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), FareServiceModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
